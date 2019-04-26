ce135f0dd470b7e8d02a939ce9c426476feffeb2
Rob <robpeterman573@gmail.com>
2019-04-26 13:01:54 -0400

    added socket io to allow users to see scan status

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,44 @@
 import React from 'react';
-import {API_BASE_URL} from '../config';
+import {
+  API_BASE_URL
+} from '../config';
+import io from "socket.io-client";
 import Alert from 'react-s-alert';
-
+import moment from 'moment';
 
 export default class Header extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {
+      super(props);
+      this.state = {
+        scanData: {
+          running: false
         }
+      };
+
     }
-handleScanClick(){
-  fetch(`${API_BASE_URL}/initiate-scan`).then((result)=>{
-    if(result.statusText === 'OK'){
-      Alert.success(`Scan Initiated - Est. Time to Complete: 5 Mins`, {
-        position: 'top-right',
-        effect: 'slide',
-        timeout: 3000,
-        offset: 100,
-      });
-  }
-  else{
-    Alert.warning(`Scan Already Running`, {
-      position: 'top-right',
-      effect: 'slide',
-      timeout: 3000,
-      offset: 100,
-    });
-  }
-  })
-}
+    componentDidMount() {
+      this.scanSocket = io(API_BASE_URL);
+      this.scanSocket.on("scan-update", msg => {
+        this.setState({
+          scanData: msg
+        })
+      })
+    }
+
+    handleScanClick() {
+      if (this.state.scanData.running) {
+
+        Alert.warning('Scan Already Running', {
+          position: 'top-right',
+          effect: 'slide',
+          timeout: 3000,
+          offset: 100
+        });
+        return;
+      }
+      this.scanSocket.emit('test', 'test message')
+    }
+
   render() {
 
     return (
@@ -36,9 +46,15 @@ handleScanClick(){
 
             <h1 className='header__h1'>HR DATA MONITOR</h1>
 
-            <div className='header__scan-section'>  
-              <h3 className='header__last-scan'>Last Scan: {this.props.lastScan}</h3>
-              <button onClick={()=>{this.handleScanClick()}} className='header__scan-btn'>Scan</button>
+            <div className='header__scan-section'>
+
+              <div> 
+                <div><b>Last Scan: </b> {moment(Number(this.props.lastScan)).format('M/DD LTS')}</div>
+                <div><b>Scan Status: </b>{this.state.scanData.running ? `Running - ${this.state.scanData.scanLength - this.state.scanData.scansLeft}/${this.state.scanData.scanLength} sheets` : 'Inactive'}</div>
+              </div>
+            
+           <button onClick={()=>{this.handleScanClick()}} className='header__scan-btn'>Scan</button>
+            
             </div>
 
         </div>
